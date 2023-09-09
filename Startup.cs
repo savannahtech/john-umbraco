6a38dc7816dcaef6ee784c56d9e8cf8a02fbfe31
John Edet <jedet@axxess.com>
2023-09-09 12:18:12 +0100

    include assets

--- Startup.cs
+++ Startup.cs
@@ -44,10 +44,10 @@ namespace UmbracoProject
         /// <param name="env">The web hosting environment.</param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
-            }
+            //}
 
             app.UseUmbraco()
                 .WithMiddleware(u =>
